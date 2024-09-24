a17a9aed3288abfc526b0f4187d336fd9b7e6f0d
Breno Brandão <brenoca35@gmail.com>
2024-09-24 13:19:29 -0300

    Query cliente + actions

---
+++ Codigo/src/routes/clientes/+page.server.ts
@@ -0,0 +1,31 @@
+import { clienteController } from '$lib/db/controllers';
+import type { PageServerLoad, Actions } from './$types';
+
+export const load = (async () => {
+	const clientes = await clienteController.selectAllClientes();
+	return { clientes };
+}) satisfies PageServerLoad;
+
+export const actions: Actions = {
+	create: async ({ request }) => {
+		const data = await request.formData();
+		const name = String(data.get('name'));
+		const cnpj = String(data.get('cnpj'));
+
+		await clienteController.insertCliente({ name, cnpj });
+	},
+	delete: async ({ request }) => {
+		const data = await request.formData();
+		const id = Number(data.get('id'));
+
+		await clienteController.deleteCliente(id);
+	},
+	update: async ({ request }) => {
+		const data = await request.formData();
+		const id = Number(data.get('id'));
+		const name = String(data.get('name'));
+		const cnpj = String(data.get('cnpj'));
+
+		await clienteController.updateCliente(id, { name, cnpj });
+	}
+};

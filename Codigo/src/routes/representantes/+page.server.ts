b0fc6cc8054ebabd899632bb2f7153c31e9c67e0
niicolasaps <nicolasalmeida.ps@gmail.com>
2024-09-24 19:02:52 -0300

    pagina representante

--- Codigo/src/routes/representantes/+page.server.ts
+++ Codigo/src/routes/representantes/+page.server.ts
@@ -1,5 +1,28 @@
-import type { PageServerLoad } from './$types';
+import { clienteController, representanteController } from '$lib/db/controllers';
+import type { PageServerLoad, Actions } from './$types';
 
-export const load = (async () => {
-    return {};
+export const load = (async () => {const representantes = representanteController.selectAllRepresentantes();
+	return { representantes };
 }) satisfies PageServerLoad;
+
+export const actions: Actions = {
+	create: async ({ request }) => {
+		const data = await request.formData();
+		const name = String(data.get('name'));
+
+		await representanteController.insertRepresentante({ name });
+	},
+	delete: async ({ request }) => {
+		const data = await request.formData();
+		const id = Number(data.get('id'));
+
+		await representanteController.deleteRepresentante(id);
+	},
+	update: async ({ request }) => {
+		const data = await request.formData();
+		const id = Number(data.get('id'));
+		const name = String(data.get('name'));
+
+		await representanteController.updateRepresentante(id, { name });
+	}
+};

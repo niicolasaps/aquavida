e4edbed598f02cfd96effb86a3ceb18e9279f6f6
Breno Brandão <brenoca35@gmail.com>
2024-09-24 13:52:38 -0300

    value cliente

--- Codigo/src/routes/clientes/+page.svelte
+++ Codigo/src/routes/clientes/+page.svelte
@@ -174,10 +174,10 @@
 							d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z"
 						/>
 					</svg>
-					<input type="text" class="grow" placeholder="Name" name="name" />
+					<input type="text" class="grow" placeholder="Name" name="name" value={selectedCliente.name}/>
 				</label>
 				<label class="input input-bordered flex items-center gap-2">
-					<input type="text" class="grow" placeholder="Cnpj" name="cnpj" />
+					<input type="text" class="grow" placeholder="Cnpj" name="cnpj" value={selectedCliente.cnpj} />
 				</label>
 				<button class="btn btn-info" type="submit">Atualizar cliente</button>
 			</div>

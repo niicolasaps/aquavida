52ea195a4d98ab1fd4706506e03d0838f3919285
Breno Brandão <brenoca35@gmail.com>
2024-09-26 16:15:38 -0300

    login cliente com endereco

--- Codigo/src/routes/(authentication)/signup/+page.svelte
+++ Codigo/src/routes/(authentication)/signup/+page.svelte
@@ -1,44 +1,179 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import { enhance } from '$app/forms';
+	import { getEnderecoFromCEP } from '$lib/utils/utils';
+
+	let isCliente = false;
+	let isRepresentante = false;
+
+	let formEndereco = {
+		number: '',
+		cep: '',
+		street: '',
+		complement: '',
+		neighborhood: '',
+		city: '',
+		state: ''
+	};
+
+	async function handleCep(cep: string) {
+		const responseapi = await getEnderecoFromCEP(cep);
+		if (responseapi.bairro) {
+			formEndereco.neighborhood = responseapi.bairro;
+		}
+		if (responseapi.logradouro) {
+			formEndereco.street = responseapi.logradouro;
+		}
+		if (responseapi.uf) {
+			formEndereco.state = responseapi.uf;
+		}
+		if (responseapi.localidade) {
+			formEndereco.city = responseapi.localidade;
+		}
+		console.log(responseapi);
+	}
 </script>
 
 <div class="min-h-[80vh] flex items-center justify-center">
 	<div class="w-full max-w-sm mx-auto overflow-hidden bg-base-200 rounded-lg shadow-md">
 		<div class="px-6 py-4">
-			<h3 class="mt-3 text-xl font-medium text-center text-opacity-60">Criar sua conta!</h3>
-			<form method="post" use:enhance>
-				<div class="w-full mt-4">
-					<label class="input input-bordered flex items-center gap-2">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 16 16"
-							fill="currentColor"
-							class="h-4 w-4 opacity-70"
+			{#if isCliente || isRepresentante}
+				<div class="flex justify-center">
+					<div class="badge badge-primary">
+						<button
+							class="flex justify-center gap-1 items-center"
+							on:click={() => {
+								isCliente = false;
+								isRepresentante = false;
+							}}
+							><svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="14"
+								height="14"
+								viewBox="0 0 24 24"
+								fill="none"
+								stroke="currentColor"
+								stroke-width="2"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								class="lucide lucide-undo-2"
+								><path d="M9 14 4 9l5-5" /><path
+									d="M4 9h10.5a5.5 5.5 0 0 1 5.5 5.5a5.5 5.5 0 0 1-5.5 5.5H11"
+								/></svg
+							> Voltar</button
 						>
-							<path
-								d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z"
+					</div>
+				</div>
+			{/if}
+			{#if isRepresentante}
+				<h3 class="mt-3 text-xl font-medium text-center text-opacity-60">Criar sua conta!</h3>
+				<form method="post" use:enhance>
+					<div class="w-full mt-4">
+						<label class="input input-bordered flex items-center gap-2">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 16 16"
+								fill="currentColor"
+								class="h-4 w-4 opacity-70"
+							>
+								<path
+									d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z"
+								/>
+							</svg>
+							<input type="text" class="grow" placeholder="Username" />
+						</label>
+					</div>
+					<div class="w-full mt-4">
+						<label class="input input-bordered flex items-center gap-2">
+							<input type="text" class="grow" placeholder="Email" />
+						</label>
+					</div>
+
+					<div class="w-full mt-4">
+						<label class="input input-bordered flex items-center gap-2">
+							<input type="password" class="grow" />
+						</label>
+					</div>
+
+					<button class="btn btn-primary w-full mt-4"> Registrar </button>
+				</form>
+			{:else if isCliente}
+				<h3 class="mt-3 text-xl font-medium text-center text-opacity-60">
+					Criar sua conta de Cliente!
+				</h3>
+				<form method="post" use:enhance>
+					<div class="w-full mt-4">
+						<label class="input input-bordered flex items-center gap-2">
+							<input type="text" class="grow" placeholder="Username" />
+						</label>
+					</div>
+					<div class="w-full mt-4">
+						<label class="input input-bordered flex items-center gap-2">
+							<input type="text" class="grow" placeholder="Email" />
+						</label>
+					</div>
+					<div class="w-full mt-4">
+						<label class="input input-bordered flex items-center gap-2">
+							<input type="password" class="grow" placeholder="Password" />
+						</label>
+					</div>
+					<div class="w-full mt-4 grid grid-cols-2 gap-2">
+						<label class="input input-bordered flex items-center gap-2">
+							<input
+								type="text"
+								class="grow"
+								placeholder="CEP"
+								on:change={async (e) => {
+									const value = e.target?.value;
+									if (value.length === 8) {
+										await handleCep(value);
+									}
+									console.log(value);
+								}}
+								bind:value={formEndereco.cep}
 							/>
-						</svg>
-						<input type="text" class="grow" placeholder="Username" />
-					</label>
+						</label>
+						<label class="input input-bordered flex items-center gap-2">
+							<input type="text" class="grow" placeholder="Street"bind:value={formEndereco.street} />
+						</label>
+					</div>
+					<div class="w-full mt-4 grid grid-cols-2 gap-2">
+						
+						<label class="input input-bordered flex items-center gap-2">
+							<input type="text" class="grow" placeholder="Bairro" bind:value={formEndereco.neighborhood} />
+						</label>
+						<label class="input input-bordered flex items-center gap-2">
+							<input type="text" class="grow" placeholder="City" bind:value={formEndereco.city}/>
+						</label>
+					</div>
+					<div class="w-full mt-4 grid grid-cols-2 gap-2">
+						<label class="input input-bordered flex items-center gap-2">
+							<input type="text" class="grow" placeholder="State" bind:value={formEndereco.state}/>
+						</label>
+						
+						<label class="input input-bordered flex items-center gap-2">
+							<input type="text" class="grow" placeholder="Number" bind:value={formEndereco.number}/>
+						</label>
+					</div>
+					
+					
+					<button class="btn btn-primary w-full mt-4">Registrar</button>
+				</form>
+			{:else}
+				<h3 class="mt-3 text-xl font-medium text-center text-opacity-60">
+					Qual seu tipo de usuario?
+				</h3>
+				<div class="flex flex-col gap-2 mt-3">
+					<button class="btn btn-primary w-full" on:click={() => (isCliente = !isCliente)}
+						>Cliente</button
+					>
+					<button
+						class="btn btn-primary w-full"
+						on:click={() => (isRepresentante = !isRepresentante)}>Representante</button
+					>
 				</div>
-				<div class="w-full mt-4">
-					<label class="input input-bordered flex items-center gap-2">
-						<input type="text" class="grow" placeholder="Email" />
-					</label>
-				</div>
-
-				<div class="w-full mt-4">
-					<label class="input input-bordered flex items-center gap-2">
-						<input type="password" class="grow" />
-					</label>
-				</div>
-
-				<button class="btn btn-primary w-full mt-4"> Registrar </button>
-			</form>
+			{/if}
 		</div>
-
 		<div class="flex items-center justify-center py-4 text-center bg-base-300 bg-opacity-40">
 			<span class="text-sm text-opacity-60">Já tem uma conta? </span>
 

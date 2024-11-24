b1fc7aa21760a8f79f69b02ddb4c0a11879a3cc0
LeonardoVieiraM <leonardovieiramachado@gmail.com>
2024-11-24 14:28:03 -0300

    update back relatorio

--- Codigo/src/lib/server/db/schema/relatorio/controller.ts
+++ Codigo/src/lib/server/db/schema/relatorio/controller.ts
@@ -1,36 +1,43 @@
-// import { db } from '$lib/db';
-// import { relatorioTable } from '$lib/db/schema/relatorio';
-// import type { InsertRelatorio, SelectRelatorioById, SelectAllRelatorios, UpdateRelatorio, DeleteRelatorio } from '$lib/db/schema/relatorio';
+import { db } from '$lib/server/db';
+import { relatorioTable } from '$lib/server/db/schema/relatorio';
+import type { InsertRelatorio } from '$lib/server/db/schema/relatorio';
+import { contratoTable } from '../contrato';
+import { servicoToContratoRelations } from '../servico';
 
-// // Inserir um relatório
-// async function insertRelatorio(data: InsertRelatorio) {
-//     return db.insert(relatorioTable).values(data).run();
-// }
+// Inserir um relatório
+async function insertRelatorio(data: InsertRelatorio) {
+    return db.insert(relatorioTable).values(data);
+}
 
-// // Selecionar um relatório por ID
-// async function selectRelatorioById(id: number) {
-//     return db.select().from(relatorioTable).where((relatorioTable.id as any).eq(id)).get();
-// }
+// Selecionar um relatório por ID
+async function selectRelatorioById(id: number) {
+    return db.select().from(relatorioTable).where((relatorioTable.id as any).eq(id)).get();
+}
 
-// // Selecionar todos os relatórios
-// async function selectAllRelatorios() {
-//     return db.select().from(relatorioTable).all();
-// }
+// Selecionar todos os relatórios
+async function selectAllRelatorios() {
+    return db.select().from(relatorioTable).all();
+}
 
-// // Atualizar um relatório por ID
-// async function updateRelatorio(id: number, data: Partial<InsertRelatorio>) {
-//     return db.update(relatorioTable).set(data).where((relatorioTable.id as any).eq(id)).run();
-// }
+async function selectRelatorioByContrato(id: number) {
+    return db.select().from(relatorioTable).where((relatorioTable.contrato_ID as any).eq(id)).get();
+}
 
-// // Deletar um relatório por ID
-// async function deleteRelatorio(id: number) {
-//     return db.delete(relatorioTable).where((relatorioTable.id as any).eq(id)).run();
-// }
+// Atualizar um relatório por ID
+async function updateRelatorio(id: number, data: Partial<InsertRelatorio>) {
+    return db.update(relatorioTable).set(data).where((relatorioTable.id as any).eq(id)).run();
+}
 
-// export const relatorioController = {
-//     insertRelatorio,
-//     selectRelatorioById,
-//     selectAllRelatorios,
-//     updateRelatorio,
-//     deleteRelatorio
-// };
+// Deletar um relatório por ID
+async function deleteRelatorio(id: number) {
+    return db.delete(relatorioTable).where((relatorioTable.id as any).eq(id)).run();
+}
+
+export const relatorioController = {
+    insertRelatorio,
+    selectRelatorioById,
+    selectAllRelatorios,
+    updateRelatorio,
+    deleteRelatorio,
+    selectRelatorioByContrato
+};

c973fbffccbd9e840f4e847a18a2ded81ab7057c
brenin35 <brenoca35@gmail.com>
2024-12-04 20:15:17 -0300

    enhance navbar component with dropdown menus and service modal

--- Codigo/src/lib/components/navbar/Navbar.svelte
+++ Codigo/src/lib/components/navbar/Navbar.svelte
@@ -3,18 +3,37 @@
 	import { getUserContext } from '$lib/stores/user';
 	import { enhance } from '$app/forms';
 	import type { SelectServico, SelectUser } from '$lib/server/db/schema';
+	import CirclePlus from 'lucide-svelte/icons/circle-plus';
+	import Cloud from 'lucide-svelte/icons/cloud';
+	import CreditCard from 'lucide-svelte/icons/credit-card';
+	import Github from 'lucide-svelte/icons/github';
+	import Keyboard from 'lucide-svelte/icons/keyboard';
+	import LifeBuoy from 'lucide-svelte/icons/life-buoy';
+	import LogOut from 'lucide-svelte/icons/log-out';
+	import Mail from 'lucide-svelte/icons/mail';
+	import MessageSquare from 'lucide-svelte/icons/message-square';
+	import Plus from 'lucide-svelte/icons/plus';
+	import Settings from 'lucide-svelte/icons/settings';
+	import User from 'lucide-svelte/icons/user';
+	import UserPlus from 'lucide-svelte/icons/user-plus';
+	import Users from 'lucide-svelte/icons/users';
+
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+	import { buttonVariants } from '$lib/components/ui/button/index.js';
+
+	import { DropletIcon, LeafIcon, ZapIcon, ShieldIcon } from 'lucide-svelte';
 
 	const user = getUserContext();
-	export let fullUser: SelectUser;
-	export let servicos: SelectServico[];
+
+	let { fullUser, servicos }: { fullUser: SelectUser; servicos: SelectServico[] } = $props();
 
 	let isOpenModal: HTMLDialogElement | null = null;
 </script>
 
+<!--
 <nav class="bg-base-200">
 	<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
 		<a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-			<!-- <img src="logo" class="h-8" alt="Aqua vida logo" /> -->
 			<span class="self-center text-2xl font-semibold whitespace-nowrap">Aqua vida</span>
 		</a>
 		<div class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse gap-2">
@@ -38,13 +57,9 @@
 					</div>
 				</div>
 				<ThemeSwitch />
-				<!-- {$user.username}
-				<form method="post" use:enhance>
-					<button>Sign out</button>
-				</form> -->
+				
 			{:else}
 				<a href="/login" class="btn btn-primary">Login</a>
-				<!-- <a href="/signup">cadastrar</a> -->
 			{/if}
 
 			<button
@@ -133,6 +148,126 @@
 		</div>
 	</div>
 </nav>
+
+<dialog id="my_modal_2" class="modal" bind:this={isOpenModal}>
+	<div class="modal-box overflow-auto">
+		<h1 class="mb-3 text-3xl font-semibold text-center">Nossos serviços</h1>
+		{#if servicos.length > 0}
+			<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+				{#each servicos as servico (servico.id)}
+					<div class="bg-base-200 rounded-lg shadow-md p-4 hover:shadow-lg transition-shadow">
+						<h2 class="text-2xl font-semibold mb-2">{servico.name}</h2>
+						<p class="">{servico.description}</p>
+					</div>
+				{/each}
+			</div>
+		{:else}
+			<h1 class="text-center text-lg">Nenhum serviço ainda!</h1>
+		{/if}
+	</div>
+	<form method="dialog" class="modal-backdrop">
+		<button>close</button>
+	</form>
+</dialog>
+
+<style>
+	.modal-box {
+		width: 100% !important;
+		max-width: 200vh !important;
+	}
+</style> -->
+<div class="bg-base-200">
+	<header class="h-14 flex items-center container ">
+		<a class="flex items-center justify-center gap-4" href="/">
+			<DropletIcon class="h-6 w-6" />
+			<span class="self-center text-2xl font-semibold whitespace-nowrap">Aqua vida</span>
+		</a>
+		<nav class="ml-auto flex gap-4 sm:gap-6 items-center">
+			<a class="text-sm font-medium hover:underline underline-offset-4" on:click={() => isOpenModal?.showModal()}>
+				Todos serviços
+			</a>
+	
+			
+			{#if $user}
+	
+			{#if fullUser.tipo === 'gerente'}
+						<div class="dropdown">
+							<div tabindex="0" role="button" class="text-sm font-medium hover:underline underline-offset-4">Admin</div>
+							<ul
+								tabindex="0"
+								class="dropdown-content menu bg-base-100 rounded-box z-[90] w-52 p-2 shadow"
+							>
+								<li><a href="/admin/clientes">Clientes</a></li>
+								<li><a href="/admin/representantes">Representantes</a></li>
+								<li><a href="/admin/servicos">Serviços</a></li>
+								<li><a href="/admin/pedidos">Pedidos</a></li>
+								<li><a href="/admin/template">Criar templates</a></li>
+								<li><a href="/admin/contratos">Contratos</a></li>
+							</ul>
+						</div>
+					{/if}
+
+					{#if fullUser.tipo === 'cliente'}
+						<div class="dropdown">
+							<div tabindex="0" role="button" class="text-sm font-medium hover:underline underline-offset-4">Cliente</div>
+							<ul
+								tabindex="0"
+								class="dropdown-content menu bg-base-100 rounded-box z-[90] w-52 p-2 shadow"
+							>
+								<li><a href="/customer/solicitar">Solicitar serviço</a></li>
+								<li><a href="/customer/contratos/1">Visualizar contratos</a></li>
+							</ul>
+						</div>
+					{/if}
+					{#if fullUser.tipo === 'representante'}
+						<div class="dropdown">
+							<div tabindex="0" role="button" class="text-sm font-medium hover:underline underline-offset-4">Representante</div>
+							<ul
+								tabindex="0"
+								class="dropdown-content menu bg-base-100 rounded-box z-[90] w-52 p-2 shadow"
+							>
+								<li><a href="/representantes/clientes">Seus clientes</a></li>
+							</ul>
+						</div>
+					{/if}
+	
+				<DropdownMenu.Root>
+					<DropdownMenu.Trigger class="text-sm font-medium underline-offset-4 btn btn-sm btn-secondary"
+						>{$user.username}</DropdownMenu.Trigger
+					>
+					<DropdownMenu.Content class="w-56">
+						<DropdownMenu.Group>
+							<DropdownMenu.GroupHeading>Meu perfil</DropdownMenu.GroupHeading>
+							<DropdownMenu.Separator />
+							<DropdownMenu.Group>
+								<a href="/profile">
+									<DropdownMenu.Item>
+										<User class="mr-2 size-4" />
+										<span>Profile</span>
+									</DropdownMenu.Item>
+								</a>
+							</DropdownMenu.Group>
+	
+							<form method="post" action="/login?/logout" class="w-full">
+								<button type="submit" class="w-full">
+									<DropdownMenu.Item>
+										<LogOut class="mr-2 size-4" />
+										<span>Log out</span>
+									</DropdownMenu.Item>
+								</button>
+							</form>
+						</DropdownMenu.Group>
+					</DropdownMenu.Content>
+				</DropdownMenu.Root>
+				
+			{:else}
+				<a href="/login" class="text-sm font-medium hover:underline underline-offset-4">Login</a>
+				
+			{/if}
+			<ThemeSwitch />
+		</nav>
+	</header>
+</div>
 
 <dialog id="my_modal_2" class="modal" bind:this={isOpenModal}>
 	<div class="modal-box overflow-auto">
--- Codigo/src/lib/components/navbar/ThemeSwitch.svelte
+++ Codigo/src/lib/components/navbar/ThemeSwitch.svelte
@@ -1,13 +1,14 @@
 <script>
 	import { themes, changeTheme } from '$lib';
+	import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
 </script>
 
 <div class="dropdown dropdown-end dropdown-bottom">
-	<div title="Themes" role="button" tabindex="0" class="btn bg-base-300">
+	<div title="Themes" role="button" tabindex="0" class="text-sm  underline-offset-4 flex gap-1 items-center btn btn-primary btn-sm">
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
-			width="24"
-			height="24"
+			width="18"
+			height="18"
 			viewBox="0 0 24 24"
 			fill="none"
 			stroke="currentColor"
@@ -28,10 +29,10 @@
 			/><path
 				d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10c.926 0 1.648-.746 1.648-1.688 0-.437-.18-.835-.437-1.125-.29-.289-.438-.652-.438-1.125a1.64 1.64 0 0 1 1.668-1.668h1.996c3.051 0 5.555-2.503 5.555-5.554C21.965 6.012 17.461 2 12 2z"
 			/></svg
-		>Mudar tema
+		>
+		Mudar tema
 	</div>
 
-	<!-- Themes -->
 	<div
 		class="dropdown-content z-10 mt-2 flex h-72 w-64 flex-col gap-2 overflow-y-scroll rounded-box bg-base-200 p-4 shadow-2xl"
 	>
